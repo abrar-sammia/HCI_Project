a0f5abc62fb28478a660792cb12933a0330b9358
Steven Ho <steven.ho2@ucalgary.ca>
2016-11-15 16:16:16 -0700

    Fixed the carmer -needs to be on kitkat

--- Timber-AGardeningGuide/app/src/main/java/com/example/sammiaabrar/timber_agardeningguide/PhotoSearch.java
+++ Timber-AGardeningGuide/app/src/main/java/com/example/sammiaabrar/timber_agardeningguide/PhotoSearch.java
@@ -1,6 +1,7 @@
 package com.example.sammiaabrar.timber_agardeningguide;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -29,33 +30,33 @@ public class PhotoSearch extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getFile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+//                File file = getFile();
+//                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(camera_intent,CAM_REQUEST);
 
             }
         });
 
     }
-
-    private File getFile(){
-
-        File folder = new File("sdcard/Timber/Pictures");
-
-        if (!folder.exists()){
-            folder.mkdir();
-        }
-
-        File image_file = new File(folder,"plant_image.jpg");
-
-        return image_file;
-    }
+//
+//    private File getFile(){
+//
+//        File folder = new File("sdcard/Timber/Pictures");
+//
+//        if (!folder.exists()){
+//            folder.mkdir();
+//        }
+//
+//        File image_file = new File(folder,"plant_image.jpg");
+//
+//        return image_file;
+//    }
 
     @Override
-    protected void onActivityResult (int requestCode, int resuiltCode, Intent data){
-        String path = "sdcard/Timber/Pictures/plant_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(thumbnail);
     }
-
 }
