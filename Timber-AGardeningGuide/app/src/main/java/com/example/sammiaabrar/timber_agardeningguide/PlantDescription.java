b7e9cdd18d97ff0be8235e08a55aef9ff97c4e2c
Steven Ho <steven.ho2@ucalgary.ca>
2016-11-26 21:54:04 -0700

    Plant description is done.

--- Timber-AGardeningGuide/app/src/main/java/com/example/sammiaabrar/timber_agardeningguide/PlantDescription.java
+++ Timber-AGardeningGuide/app/src/main/java/com/example/sammiaabrar/timber_agardeningguide/PlantDescription.java
@@ -6,8 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PlantDescription extends AppCompatActivity {
+
+    private ArrayList<Integer> symbol = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,9 @@ public class PlantDescription extends AppCompatActivity {
         setContentView(R.layout.activity_plant_description);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        createSymbols();
+        populateSymbols();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +34,71 @@ public class PlantDescription extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void createSymbols() {
+        this.symbol.add(R.drawable.light_1);
+        this.symbol.add(R.drawable.light_2);
+        this.symbol.add(R.drawable.light_3);
+        this.symbol.add(R.drawable.light_4);
+        this.symbol.add(R.drawable.mist_1);
+        this.symbol.add(R.drawable.plant_height_1);
+        this.symbol.add(R.drawable.plant_height_2);
+        this.symbol.add(R.drawable.plant_height_3);
+        this.symbol.add(R.drawable.potted_1);
+        this.symbol.add(R.drawable.temp_1);
+        this.symbol.add(R.drawable.temp_2);
+        this.symbol.add(R.drawable.temp_3);
+        this.symbol.add(R.drawable.water_1);
+        this.symbol.add(R.drawable.water_2);
+        this.symbol.add(R.drawable.water_3);
+        this.symbol.add(R.drawable.water_4);
+    }
+
+    private void populateSymbols(){
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.symbol_row);
+
+        int size = randomNum(1,7);
+        int symbol_size = (symbol.size() - 1);
+        ArrayList<Integer> symbol_id = new ArrayList<Integer>();
+
+        for(int i = 0, temp = 0,temp2 = 0; i < size; i++){
+
+            if(symbol_id.size() == 0){
+                temp = randomNum(0,symbol_size);
+                symbol_id.add(symbol.get(temp));
+            }
+            else {
+
+                temp = randomNum(0,symbol_size);
+                temp2 = symbol.get(temp);
+
+                if (symbol_id.contains(temp2)){
+                   i--;
+                }
+                else{
+                    symbol_id.add(temp2);
+                }
+
+            }
+        }
+
+        for (int i = 0; i < size; i++){
+
+            Button new_Btn = new Button(this);
+            new_Btn.setLayoutParams(new LinearLayout.LayoutParams(250,250));
+            new_Btn.setBackgroundResource(symbol_id.get(i));
+            layout.addView(new_Btn);
+
+        }
+
+    }
+
+    private int randomNum(int min, int max){
+
+        Random random = new Random();
+
+        return random.nextInt((max - min) + 1) + min;
     }
 }
